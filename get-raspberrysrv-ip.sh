24f142474225f64ec271819b6185cc7291aa17f9
virtueistheonlygood <virtueistheonlygood@gmail.com>
2023-04-30 03:11:38 +0000

    update script get-raspberrysrv-ip.sh

--- get-raspberrysrv-ip.sh
+++ get-raspberrysrv-ip.sh
@@ -1,4 +1,4 @@
 echo -n "WireGuard PIA IP: "
 ssh pi@raspberrysrv ip a show dev pia|grep inet|awk '{print$2}'|sed 's|/32||'
-echo -n "OpenVPN PIA IP: "
+echo -n "OpenVPN TUN IP: "
 ssh pi@raspberrysrv ip a show dev tun0|grep inet|awk '{print$2}'|sed 's|/24||'
