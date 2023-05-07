cf3b5dbb851cc3e6ae1cc9a157ddc5cdd0251f2b
virtueistheonlygood <virtueistheonlygood@gmail.com>
2023-05-07 02:38:01 +0000

    update raspinet.py

--- get-raspberrysrv-ip.sh
+++
@@ -1,4 +0,0 @@
-echo -n "WireGuard PIA IP: "
-ssh pi@raspberrysrv ip a show dev pia|grep inet|awk '{print$2}'|sed 's|/32||'
-echo -n "OpenVPN TUN IP: "
-ssh pi@raspberrysrv ip a show dev tun0|grep inet|awk '{print$2}'|sed 's|/24||'
