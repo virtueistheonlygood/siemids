b3444d422a9c41b6d5e0bc22952b85375e3aec8d
skynet <virtueistheonlygood@proton.me>
2023-08-16 23:00:39 +0200

    elitebook saved objects update

--- network-watcher.py
+++ network-watcher.py
@@ -73,26 +73,3 @@ with open('env/pianet.env', 'w') as f:
         else:
             f.write(f'{key.upper()}={value}\n')
 
-import docker
-import os
-
-# Create a Docker client object
-client = docker.from_env()
-
-# Find the container by name
-container = client.containers.get('siemids_filebeat-elitebook_1')
-
-# Stop the container
-container.stop()
-
-# Delete the container
-container.remove()
-
-# Change to the directory containing the docker-compose.yml file
-os.chdir('/home/skynet/siemids/')
-
-# Clean old containers
-os.system('docker system prune -f')
-
-# Start the services defined in docker-compose.yml
-os.system('docker-compose up -d')
