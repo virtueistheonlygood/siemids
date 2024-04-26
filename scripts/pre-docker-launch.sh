9dd16b938546b7a4dc5fa61ee3c8214c0e4440e4
root <root@elitebook.skynet>
2024-04-26 16:00:01 -0300

    update project

--- scripts/pre-docker-launch.sh
+++ scripts/pre-docker-launch.sh
@@ -1,3 +1,3 @@
 sudo sysctl -w vm.max_map_count=262144
 cd /home/skynet/github-repos/siemids/scripts
-sudo chown root:root ../resources/filebeat*.yml ../resources/suricata* 
+sudo chown root:root ../resources/filebeat/filebeat*.yml ../resources/suricata/suricata* 
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo sh -x scritps/prune.sh
+sudo sh -x scripts/pre-docker-launch.sh
 python3 scripts/deploy.py
